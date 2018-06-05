3997b7c4e35924555334c1f898592625bccb0f2a
Milan Gupta <milan.gupta.iitd@gmail.com>
2018-06-05 11:02:19 +0530

    Indexed all variable names in index.php

--- index.php
+++ index.php
@@ -10,13 +10,13 @@
   include('./vdo_play.php');
   include('./vdo_otp.php');
   // Initializes client key, video ID and OTP details such as annotation code and URL, IP-geo restrictionss
-  $video_details = vdo_init();
+  $vdo_video_details = vdo_init();
   // Calls and receives OTP json object
-  $otp_response = vdo_otp($video_details);
+  $vdo_otp_response = vdo_otp($vdo_video_details);
   // Uses returned OTP to create embed code
-  $embed_code = vdo_play($otp_response);
+  $vdo_embed_code = vdo_play($vdo_otp_response);
   //Displays the embed code
-  echo $embed_code;
+  echo $vdo_embed_code;
 ?>
 </body>
 </html>
